11c2fb01ac8f24d17e021bf2b340b43b56621862
wuhao <2478731248@qq.com>
2020-12-21 22:47:47 +0800

    水平方向布局

--- lib/main.dart
+++ lib/main.dart
@@ -5,89 +5,46 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final List<String> items;
-  MyApp({Key key, @required this.items}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "gridView网格列表",
+        title: '水平方向布局',
         home: Scaffold(
           appBar: AppBar(
-            title: Text('gridView网格列表'),
+            title: Text('水平方向布局'),
           ),
-          body: GridView(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 2.0,
-                crossAxisSpacing: 2.0,
-                childAspectRatio: 0.7),
+          body: new Row(
             children: <Widget>[
-              Image.network(
-                'http://img5.mtime.cn/mt/2020/10/10/111703.25015879_180X260X4.jpg',
-                fit: BoxFit.cover,
+              Expanded(
+                child: RaisedButton(
+                  onPressed: () {},
+                  color: Colors.red,
+                  child: Text('red button '),
+                ),
               ),
-              Image.network(
-                'http://img5.mtime.cn/mt/2020/12/15/123841.10782003_180X260X4.jpg',
-                fit: BoxFit.cover,
+              Expanded(
+                child: RaisedButton(
+                  onPressed: () {},
+                  color: Colors.yellow,
+                  child: Text('yellow button '),
+                ),
               ),
-              Image.network(
-                'http://img5.mtime.cn/mt/2020/09/10/091215.17558114_180X260X4.jpg',
-                fit: BoxFit.cover,
+              Expanded(
+                child: RaisedButton(
+                  onPressed: () {},
+                  color: Colors.blue,
+                  child: Text('blue button '),
+                ),
               ),
-              Image.network(
-                'http://img5.mtime.cn/mt/2020/08/26/164823.42526363_180X260X4.jpg',
-                fit: BoxFit.cover,
-              ),
-              Image.network(
-                'http://img5.mtime.cn/mt/2017/10/31/151824.97647389_135X190X4.jpg',
-                fit: BoxFit.cover,
-              ),
-              Image.network(
-                'http://img5.mtime.cn/mt/2020/10/01/115233.44324693_135X190X4.jpg',
-                fit: BoxFit.cover,
-              ),
-              Image.network(
-                'http://img5.mtime.cn/mt/2020/08/07/112210.24896484_135X190X4.jpg',
-                fit: BoxFit.cover,
-              ),
-              Image.network(
-                'http://img5.mtime.cn/mt/2020/11/26/101617.43378907_135X190X4.jpg',
-                fit: BoxFit.cover,
-              ),
-              Image.network(
-                'http://img5.mtime.cn/mt/2020/01/08/144722.88106930_135X190X4.jpg',
-                fit: BoxFit.cover,
+              Expanded(
+                child: RaisedButton(
+                  onPressed: () {},
+                  color: Colors.orange,
+                  child: Text('orange button'),
+                ),
               ),
             ],
           ),
         ));
   }
 }
-
-//横向list组件
-// class MyList extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView(
-//       scrollDirection: Axis.horizontal, //横向
-//       children: <Widget>[
-//         Container(
-//           width: 180,
-//           color: Colors.lightBlue,
-//         ),
-//         Container(
-//           width: 180,
-//           color: Colors.yellow,
-//         ),
-//         Container(
-//           width: 180,
-//           color: Colors.orange,
-//         ),
-//         Container(
-//           width: 180,
-//           color: Colors.red,
-//         )
-//       ],
-//     );
-//   }
-// }
