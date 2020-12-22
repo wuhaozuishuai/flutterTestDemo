0d2135ca5ea80e61fc1907fbdbe6b8729208d839
wuhao <2478731248@qq.com>
2020-12-22 21:44:24 +0800

    路由跳转，传递参数

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,64 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_app_test/main1.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: "导航演示",
-    home: FirstScreen(),
-  ));
+//D:\Program Files\Nox\bin
+// nox_adb.exe connect 127.0.0.1:62001
+// import 'package:flutter_app_test/main1.dart';
+class Product {
+  final String title; //商品标题
+  final String description; //商品描述
+  Product(this.title, this.description);
 }
 
-class FirstScreen extends StatelessWidget {
+void main(List<String> args) {
+  runApp(MaterialApp(
+      title: '导航数据传递接受',
+      home: ProductList(
+        products: List.generate(
+            20, (index) => Product("商品 $index", '商品描述，编号：$index')),
+      )));
+}
+
+class ProductList extends StatelessWidget {
+  final List<Product> products;
+  ProductList({Key key, @required this.products}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('导航页面'),
+        title: Text('商品列表'),
       ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('查看详情页面'),
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => secondScreen(), //跳转的路由
-                ));
-          },
-        ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(products[index].title),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        ProductDetail(product: products[index]),
+                  ));
+            },
+          );
+        },
       ),
     );
   }
 }
 
-class secondScreen extends StatelessWidget {
+class ProductDetail extends StatelessWidget {
+  final Product product;
+  // const ProductDetail({Key key}) : super(key: key);
+  ProductDetail({Key key, @required this.product}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('详情页面'),
-        ),
-        body: Center(
-          child: RaisedButton(
-            child: Text('返回'),
-            onPressed: () {
-              Navigator.pop(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => FirstScreen(), //跳转的路由
-                  ));
-            },
-          ),
-        ));
+      appBar: AppBar(
+        title: Text('商品详情，${product.title}'),
+      ),
+      body: Center(child: Text('${product.description}')),
+    );
   }
 }
